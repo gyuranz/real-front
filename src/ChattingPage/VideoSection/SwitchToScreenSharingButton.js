5d5d59126af9ba3b160bf3fe0bf438c56ec03375
bigperson <daein7076@gmail.com>
2023-08-06 21:08:55 +0900

    screen shot is completed

--- src/ChattingPage/VideoSection/SwitchToScreenSharingButton.js
+++ src/ChattingPage/VideoSection/SwitchToScreenSharingButton.js
@@ -7,7 +7,6 @@ import { AuthLogin } from "../../atoms";
 import { useRecoilValue } from "recoil";
 import { styled } from "styled-components";
 // import { useNavigate } from "react-router-dom";
-import "../RoomPage.css";
 
 const constraints = {
     audio: false,
@@ -46,7 +45,6 @@ const SwitchToScreenSharingButton = () => {
                 setIsScreenSharingActive(true);
                 // execute here function to switch the video track which we are sending to other users
             }
-            console.log("✅✅", stream);
         } else {
             webRTCHandler.toggleScreenShare(isScreenSharingActive);
             setIsScreenSharingActive(false);
@@ -57,75 +55,45 @@ const SwitchToScreenSharingButton = () => {
         }
     };
     //! capture ~ alt(18) + a(65)
+    console.log("✅", stream);
 
     // sharedVideo.addEventListener("contextmenu", async function (event) {
     async function screenshare() {
-        // event.preventDefault();
         try {
-            // const video = document.createElement("video");
-            // video.srcObject = screenSharingStream;
-            // console.log(video);
-            // const track = await video.srcObject.getVideoTracks()[0];
-            const track = await screenSharingStream.getVideoTracks()[0];
-            // const copyTrack = track.clone();
-            console.log("✅", track);
-            const newStream = new MediaStream();
-            newStream.addTrack(track.clone());
+            const video = document.createElement("video");
+            video.srcObject = screenSharingStream;
 
-            const imageCapture = new ImageCapture(track);
-            // const imageCapture = new ImageCapture(copyTrack);
-            const bitmap = await imageCapture.grabFrame();
-            // await track.stop();
-            // await copyTrack.stop();
-            const canvas = document.getElementById("screenshot");
+            video.onloadedmetadata = async () => {
+                const canvas = document.getElementById("screenshot");
 
-            canvas.width = bitmap.width;
-            canvas.height = bitmap.height;
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
 
-            const context = canvas.getContext("2d");
-            context.drawImage(bitmap, 0, 0, bitmap.width, bitmap.height);
-            const image = canvas.toDataURL();
-            const res = await fetch(image);
-            const buff = await res.arrayBuffer();
+                const context = canvas.getContext("2d");
+                context.drawImage(
+                    video,
+                    0,
+                    0,
+                    video.videoWidth,
+                    video.videoHeight
+                );
 
-            const jpg = [
-                new File([buff], `photo_${new Date()}.jpg`, {
-                    type: "image/jpeg",
-                }),
-            ];
-            const file = new FormData();
-            file.append("file", jpg[0]); // 파일, 파일 이름 추가
+                const image = canvas.toDataURL();
+                const res = await fetch(image);
+                const buff = await res.arrayBuffer();
 
-            postScreenShot(userState.currentRoom.room_id, file);
+                const jpg = [
+                    new File([buff], `photo_${new Date()}.jpg`, {
+                        type: "image/jpeg",
+                    }),
+                ];
+                const file = new FormData();
+                file.append("file", jpg[0]); // 파일, 파일 이름 추가
 
-            // const video = document.createElement("video");
-            // video.classList.add("videoabc");
-            // video.srcObject = screenSharingStream;
-            // console.log(video.videoWidth, video.videoHeight);
+                postScreenShot(userState.currentRoom.room_id, file);
+            };
 
-            // // const canvas = document.createElement("canvas");
-            // const canvas = document.getElementById("screenshot");
-            // canvas.appendChild(video);
-            // canvas.width = video.videoWidth;
-            // canvas.height = video.videoHeight;
-            // const context = canvas.getContext("2d");
-
-            // video.onloadedmetadata = async () => {
-            //     context.drawImage(video, 0, 0, canvas.width, canvas.height);
-            //     const image = canvas.toDataURL();
-            //     const res = await fetch(image);
-            //     const buff = await res.arrayBuffer();
-
-            //     const jpg = [
-            //         new File([buff], `photo_${new Date()}.jpg`, {
-            //             type: "image/jpeg",
-            //         }),
-            //     ];
-            //     const file = new FormData();
-            //     file.append("file", jpg[0]);
-
-            //     postScreenShot(userState.currentRoom.room_id, file);
-            // };
+            video.play();
         } catch (error) {
             console.log("❌", error);
             // navigate("/");
