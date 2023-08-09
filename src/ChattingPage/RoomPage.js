1f1cab9f325af507a94280e27088595e8cc9b324
bigperson <daein7076@gmail.com>
2023-08-10 00:26:45 +0900

    modified copy

--- src/ChattingPage/RoomPage.js
+++ src/ChattingPage/RoomPage.js
@@ -66,20 +66,7 @@ const RoomPage = ({
         }
     }, []);
     const copyRoomId = () => {
-        // 임시의 textarea 생성
-        const textarea = document.createElement("textarea");
-
-        // body 요소에 존재해야 복사가 진행됨
-        document.body.appendChild(textarea);
-
-        // 복사할 특정 텍스트를 임시의 textarea에 넣어주고 모두 셀렉션 상태
-        textarea.value = userState.currentRoom.room_id;
-        console.log(textarea.value);
-        textarea.select();
-
-        // 복사 후textarea 지우기
-        document.execCommand("copy");
-        document.body.removeChild(textarea);
+        navigator.clipboard.writeText(userState.currentRoom.room_id);
     };
 
     return (
